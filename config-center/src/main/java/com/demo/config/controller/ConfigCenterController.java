736098da3a68ce9c753ba7919689bff040498de8
zhjb1025 <benny.zhang@wolaidai.com>
2017-08-19 21:42:02 +0800

    增加config-center 工程

---
+++ config-center/src/main/java/com/demo/config/controller/ConfigCenterController.java
@@ -0,0 +1,7 @@
+package com.demo.config.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigCenterController {
+}
---
+++ config-center/src/main/java/com/demo/config/service/ConfigCenterService.java
@@ -0,0 +1,68 @@
+package com.demo.config.service;
+
+import com.demo.zookeeper.Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+@Service
+public class ConfigCenterService {
+    private static Logger logger = LoggerFactory.getLogger(ConfigCenterService.class);
+
+    @Autowired
+    private Client client;
+
+    private final String lockPath = "/root/config-center/lock";
+    private final String urlPath = "/root/config-center/url";
+
+
+    @PostConstruct
+    public void init() {
+        try {
+            logger.info("本机IP地址：", getLocalRealIp());
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getLocalRealIp() throws SocketException {
+        String localip = null;// 本地IP，如果没有配置外网IP则返回它
+        String netip = null;// 外网IP
+
+        Enumeration<NetworkInterface> netInterfaces =
+                NetworkInterface.getNetworkInterfaces();
+        InetAddress ip = null;
+        boolean finded = false;// 是否找到外网IP
+        while (netInterfaces.hasMoreElements() && !finded) {
+            NetworkInterface ni = netInterfaces.nextElement();
+            Enumeration<InetAddress> address = ni.getInetAddresses();
+            while (address.hasMoreElements()) {
+                ip = address.nextElement();
+                if (!ip.isSiteLocalAddress()
+                        && !ip.isLoopbackAddress()
+                        && ip.getHostAddress().indexOf(":") == -1) {// 外网IP
+                    netip = ip.getHostAddress();
+                    finded = true;
+                    break;
+                } else if (ip.isSiteLocalAddress()
+                        && !ip.isLoopbackAddress()
+                        && ip.getHostAddress().indexOf(":") == -1) {// 内网IP
+                    localip = ip.getHostAddress();
+                }
+            }
+        }
+
+        if (netip != null && !"".equals(netip)) {
+            return netip;
+        } else {
+            return localip;
+        }
+    }
+}
