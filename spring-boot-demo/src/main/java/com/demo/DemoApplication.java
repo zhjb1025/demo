5192fe11cbe96c4817fe6b32353bf2d1e03e7b47
zhjb1025 <157141907@qq.com>
2017-08-22 17:01:14 +0800

    修改配置

--- spring-boot-demo/src/main/java/com/demo/DemoApplication.java
+++ spring-boot-demo/src/main/java/com/demo/DemoApplication.java
@@ -24,12 +24,13 @@ public class DemoApplication {
 	public static void main(String[] args) {
 	    try {
 	      log.info("开始启动服务...................");
-	      ConfigCenterClient.resetUrl(new String[] {"http://127.0.0.1:8080/config-center"});
-	      ConfigCenterClient.setGroups("all");
+	      
+//	      ConfigCenterClient.resetUrl(new String[] {"http://127.0.0.1:8080/config-center"});
+	      ConfigCenterClient.resetUrl(args[0].split(","));
+	      ConfigCenterClient.setGroups("logging,tomcat,datasource,mybatis,mongodb,actuator,zookeeper,demo");
 	      Properties properties = ConfigCenterClient.loadConfig();
 	      SpringApplication springApplication = new SpringApplication(DemoApplication.class);
 	      springApplication.setDefaultProperties(properties);
-	      //ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
 	      ApplicationContext applicationContext =springApplication.run(args);
 	      SpringContextUtil.setApplicationContext(applicationContext);
 	      log.info("成功启动服务...................");
