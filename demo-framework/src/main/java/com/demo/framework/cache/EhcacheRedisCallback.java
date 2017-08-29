1898e205ea7a2f3e2d180d78a0f9888eb8ea92d3
zhjb1025 <157141907@qq.com>
2017-08-29 10:02:01 +0800

    两级缓存，一级:ehcache,二级为redis

---
+++ demo-framework/src/main/java/com/demo/framework/cache/EhcacheRedisCallback.java
@@ -0,0 +1,116 @@
+package com.demo.framework.cache;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+
+public class EhcacheRedisCallback implements RedisCallback<Object> {
+	private static final Logger logger = LoggerFactory.getLogger(EhcacheRedisCallback.class); 
+	private String key;
+	 
+	private Object value;
+	
+	@Override
+	public Object doInRedis(RedisConnection connection) throws DataAccessException {
+		if(value==null){
+			return get(connection);
+		}else{
+			put(connection);
+			return null;
+		}
+	}
+	
+	
+	private void put(RedisConnection connection){
+        try {
+        	byte[] keyByte = key.getBytes();  
+            byte[] valueByte = toByteArray(value); 
+        	connection.set(keyByte, valueByte);
+		} catch (Exception e) {
+			logger.error("",e);
+		}
+         
+	}
+	
+	private Object get(RedisConnection connection){
+		
+		try {
+			byte[] keyByte = key.toString().getBytes();  
+	        byte[] valueByte = connection.get(keyByte);  
+	        if (valueByte == null) {  
+	            return null;  
+	        }  
+	        return toObject(valueByte);
+		} catch (Exception e) {
+			logger.error("",e);
+		}
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param obj
+	 * @return
+	 * @throws Exception
+	 */
+	private byte[] toByteArray(Object obj){  
+        byte[] bytes = null;  
+        try {
+        	ByteArrayOutputStream bos = new ByteArrayOutputStream();  
+            ObjectOutputStream oos = new ObjectOutputStream(bos);  
+            oos.writeObject(obj);  
+            oos.flush();  
+            bytes = bos.toByteArray();  
+            oos.close();
+            bos.close();
+        } catch (Exception e) {
+			logger.error("", e);
+		} 
+        return bytes;  
+    }
+	
+	
+	 /** 
+     * 描述 : byte[]转Object
+     * @param bytes 
+     * @return 
+	 * @throws IOException 
+     */  
+    private Object toObject(byte[] bytes){  
+        Object obj = null; 
+        try {  
+        	ByteArrayInputStream bis = new ByteArrayInputStream(bytes);  
+            ObjectInputStream ois = new ObjectInputStream(bis);  
+            obj = ois.readObject();
+            ois.close();  
+            bis.close();  
+        } catch (Exception e) {
+			logger.error("", e);
+		}
+        return obj;  
+    }
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public Object getValue() {
+		return value;
+	}
+
+	public void setValue(Object value) {
+		this.value = value;
+	}
+}
--- demo-framework/src/main/java/com/demo/framework/cache/RedisEhcache.java
+++ demo-framework/src/main/java/com/demo/framework/cache/RedisEhcache.java
@@ -1,16 +1,12 @@
 package com.demo.framework.cache;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.concurrent.Callable;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.Cache;
 import org.springframework.cache.support.SimpleValueWrapper;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import net.sf.ehcache.Element;
 
@@ -28,8 +24,8 @@ public class RedisEhcache implements Cache {
 
     private net.sf.ehcache.Ehcache ehcache;
 
-//    private RedisTemplate<String, Object> redisTemplate;
-
+    private StringRedisTemplate stringRedisTemplate;
+    
 	@Override
 	public String getName() {
 		logger.info("RedisEhcache.getName={}",name);
@@ -38,50 +34,86 @@ public class RedisEhcache implements Cache {
 
 	@Override
 	public Object getNativeCache() {
+		logger.info("RedisEhcache.getNativeCache");
 		return this;
 	}
 
 	@Override
 	public ValueWrapper get(Object key) {
-		logger.info("RedisEhcache.get,key={}",key);
 		Element value = ehcache.get(key);
-		return value != null ? new SimpleValueWrapper(value.getObjectValue()) : null;
+		Object result =null;
+		if(value!=null){
+			result=value.getObjectValue();
+		}else{
+			logger.info("Get value from Redis");
+			EhcacheRedisCallback callback= new EhcacheRedisCallback();
+			callback.setKey(key.toString());
+			result=stringRedisTemplate.execute(callback);
+			if(result!=null){
+				logger.info("修复本地缺失的数据key={},Value={}",key,result);
+				Element element= new Element(key, result);
+				ehcache.put(element);
+			}
+		}
+		logger.info("RedisEhcache.get,key={},Value={}",key,result);
+		return result != null ? new SimpleValueWrapper(result) : null;
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T> T get(Object key, Class<T> type) {
 		logger.info("RedisEhcache.get,key={},type={}",key,type.getName());
-		Element value = ehcache.get(key);
-		if(value==null){
+		ValueWrapper result = get(key);
+		if(result==null){
 			return null;
 		}
 		
-		return (T)value.getObjectValue();
+		return (T)result.get();
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T> T get(Object key, Callable<T> valueLoader) {
 		logger.info("RedisEhcache.get,key={},valueLoader={}",key,valueLoader.getClass().getName());
-		Element value = ehcache.get(key);
-		if(value==null){
-			return null;
+		ValueWrapper result = get(key);
+		Object val=null;
+		if(result==null){
+			try {
+				val= valueLoader.call();
+			} catch (Exception e) {
+				logger.error("",e);
+			}
+			if(val!=null){
+				logger.info("修复本地以及Redis缺失的数据 key={},Value={}",key,result);
+				put(key,val);
+			}
+		}else{
+			val=result.get();
 		}
-		return (T)value.getObjectValue();
+		logger.info("RedisEhcache.get,key={},valueLoader={},Value={}",key,valueLoader.getClass().getName(),val);
+		return (T)val;
+		
 	}
 
 	@Override
 	public void put(Object key, Object value) {
 		logger.info("RedisEhcache.put,key={},value={}",key,value);
+		EhcacheRedisCallback callback= new EhcacheRedisCallback();
+		callback.setKey(key.toString());
+		callback.setValue(value);
+		stringRedisTemplate.execute(callback);
 		Element element = new Element(key, value);
-		ehcache.putIfAbsent(element);
-
+		ehcache.put(element);
 	}
 
 	@Override
 	public ValueWrapper putIfAbsent(Object key, Object value) {
 		logger.info("RedisEhcache.putIfAbsent,key={},value={}",key,value);
+		EhcacheRedisCallback callback= new EhcacheRedisCallback();
+		callback.setKey(key.toString());
+		callback.setValue(value);
+		stringRedisTemplate.execute(callback);
+		
 		Element element = new Element(key, value);
 		ehcache.putIfAbsent(element);
 		return new SimpleValueWrapper(value); 
@@ -91,6 +123,7 @@ public class RedisEhcache implements Cache {
 	public void evict(Object key) {
 		logger.info("RedisEhcache.evict,key={}",key);
 		ehcache.remove(key);
+		stringRedisTemplate.delete(key.toString());
 
 	}
 
@@ -101,64 +134,16 @@ public class RedisEhcache implements Cache {
 
 	}
 	
-	/**
-	 * 
-	 * @param obj
-	 * @return
-	 * @throws Exception
-	 */
-	private byte[] toByteArray(Object obj) throws Exception {  
-        byte[] bytes = null;  
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();  
-        ObjectOutputStream oos = new ObjectOutputStream(bos);  
-        try {  
-            oos.writeObject(obj);  
-            oos.flush();  
-            bytes = bos.toByteArray();  
-            
-        } finally {
-        	if(oos !=null){
-        		oos.close();  
-        	}
-        	if(oos !=null){
-        		bos.close();  
-        	}
-		}  
-        return bytes;  
-    }
-	
-	
-	 /** 
-     * 描述 : byte[]转Object
-     * @param bytes 
-     * @return 
-	 * @throws IOException 
-     */  
-    private Object toObject(byte[] bytes) throws Exception {  
-        Object obj = null; 
-        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);  
-        ObjectInputStream ois = new ObjectInputStream(bis);  
-        try {  
-            obj = ois.readObject();  
-        } finally {
-        	if(ois !=null){
-        		ois.close();  
-        	}
-        	if(bis !=null){
-        		bis.close();  
-        	}
-		} 
-        return obj;  
-    }
-
 	public void setName(String name) {
 		this.name = name;
 	}
 
 	public void setEhcache(net.sf.ehcache.Ehcache ehcache) {
 		this.ehcache = ehcache;
-	} 
-    
-    
+	}
+	
 
+	public void setStringRedisTemplate(StringRedisTemplate stringRedisTemplate) {
+		this.stringRedisTemplate = stringRedisTemplate;
+	} 
 }
