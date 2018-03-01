dfeae0ab832d456ed5dbc3e1eedd0f627592ab63
zhjb1025 <157141907@qq.com>
2018-03-01 16:31:56 +0800

    redis 序列化化使用json

--- demo-framework/src/main/java/com/demo/framework/session/redis/RedisSessionService.java
+++ demo-framework/src/main/java/com/demo/framework/session/redis/RedisSessionService.java
@@ -2,11 +2,15 @@ package com.demo.framework.session.redis;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Service;
 
 import com.demo.framework.util.ThreadCacheUtil;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class RedisSessionService {
@@ -16,8 +20,19 @@ public class RedisSessionService {
 	@Autowired
 	public void setRedisTemplate(RedisTemplate<Object, Object> redisTemplate) {
 		RedisSerializer<String> stringSerializer = new StringRedisSerializer();
-		redisTemplate.setHashKeySerializer(stringSerializer);
+        // 使用Jackson2JsonRedisSerialize 替换默认序列化
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        // 设置value的序列化规则和 key的序列化规则
 		redisTemplate.setKeySerializer(stringSerializer);
+		redisTemplate.setHashKeySerializer(stringSerializer);
+		redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+		redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
 		this.redisTemplate = redisTemplate;
 	}
 
@@ -27,7 +42,6 @@ public class RedisSessionService {
 		if(value==null) {
 			return null;
 		}
-//		redisTemplate.expire(sessionKey, 60*2, TimeUnit.SECONDS);
 		return  value;
 	}
 	
---
+++ gateway/src/main/java/com/demo/gateway/RedisTemplateInit.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.demo.gateway;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author Administrator
+ *
+ */
+@Component
+public class RedisTemplateInit {
+	private  Logger logger = LoggerFactory.getLogger(this.getClass());  
+	@Autowired
+	@Qualifier("sessionRedisTemplate")
+	private  RedisTemplate<Object, Object> sessionRedisTemplate;
+	
+	@PostConstruct
+	public void init(){
+		logger.info("初始化sessionRedisTemplate的序列化类" );
+		 // 使用Jackson2JsonRedisSerialize 替换默认序列化
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        RedisSerializer<String> stringSerializer = new StringRedisSerializer();
+        // 设置value的序列化规则和 key的序列化规则
+        sessionRedisTemplate.setKeySerializer(stringSerializer);
+        sessionRedisTemplate.setHashKeySerializer(stringSerializer);
+        sessionRedisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        sessionRedisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+	}
+}
